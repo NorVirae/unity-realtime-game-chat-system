1de173728a56827e4b02d23a2830b410a22a25f5
Norbert Frank Mba <norbertmbafrank@gmail.com>
2023-06-07 05:55:30 +0100

    Add project files.

---
+++ Chat.Service/DataService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Chat.Service
+{
+    public class DataService
+    {
+        private readonly IConfiguration _configuration;
+        public DataService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        public 
+
+
+    }
+}
